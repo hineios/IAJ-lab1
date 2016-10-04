592f2edeee9dfaaa6c4207858637eb2e4db2a31f
Fábio Vieira Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2016-10-04 01:35:13 +0100

    Initial Import
    stuff made on class plus some other stuff I made at home.

---
+++ Assets/Scripts/DynamicManager.cs
@@ -0,0 +1,222 @@
+﻿using Assets.Scripts.IAJ.Unity.Movement;
+using Assets.Scripts.IAJ.Unity.Movement.DynamicMovement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DynamicManager : MonoBehaviour {
+
+    private const float X_WORLD_SIZE = 55;
+    private const float Z_WORLD_SIZE = 32.5f;
+    private const float MAX_ACCELERATION = 20.0f;
+    private const float MAX_SPEED = 20.0f;
+    private const float DRAG = 0.9f;
+
+	public DynamicCharacter RedCharacter { get; set; }
+	public DynamicCharacter GreenCharacter { get; set; }
+
+    private Text RedMovementText { get; set; }
+    private Text GreenMovementText { get; set; }
+
+    private DynamicWander RedDynamicWander { get; set; }
+    private DynamicWander GreenDynamicWander { get; set; }
+    private DynamicArrive RedDynamicArrive { get; set; }
+    private DynamicArrive GreenDynamicArrive { get; set; }
+    private DynamicSeek RedDynamicSeek { get; set; }
+    private DynamicSeek GreenDynamicSeek { get; set; }
+    private DynamicFlee RedDynamicFlee { get; set; }
+    private DynamicFlee GreenDynamicFlee { get; set; }
+    
+
+    public GameObject redDebugTarget;
+    public GameObject greenDebugTarget;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		var textObj = GameObject.Find ("InstructionsText");
+		if (textObj != null) 
+		{
+			textObj.GetComponent<Text>().text = 
+				"Instructions\n\n" +
+				"Red Character\n" +
+				"Q - Stationary\n" +
+				"W - Seek\n" + 
+				"E - Flee\n" + 
+				"R - Arrive\n" + 
+				"T - Wander\n\n" +  
+				"Green Character\n" + 
+				"A - Stationary\n" +
+				"S - Seek\n" +
+				"D - Flee\n" + 
+				"F - Arrive\n" +
+				"G - Wander\n"; 
+		}
+
+		var redObj = GameObject.Find ("Red");
+		if(redObj != null) this.RedCharacter = new DynamicCharacter(redObj)
+		{
+		    Drag = DRAG,
+            MaxSpeed = MAX_SPEED
+		};
+		var greenObj = GameObject.Find ("Green");
+		if (greenObj != null) this.GreenCharacter = new DynamicCharacter(greenObj)
+		{
+		    Drag = DRAG,
+            MaxSpeed = MAX_SPEED
+		};
+
+	    this.RedMovementText = GameObject.Find("RedMovement").GetComponent<Text>();
+	    this.GreenMovementText = GameObject.Find("GreenMovement").GetComponent<Text>();
+
+        #region movement initialization
+
+	    var redKinematicData = new KinematicData(new StaticData(this.RedCharacter.GameObject.transform.position));
+	    var greenKinematicData = new KinematicData(new StaticData(this.GreenCharacter.GameObject.transform.position));
+
+        this.RedDynamicSeek = new DynamicSeek
+        {
+            Character = redKinematicData,
+            Target = this.GreenCharacter.KinematicData,
+            MaxAcceleration = MAX_ACCELERATION
+        };
+
+        this.RedDynamicFlee = new DynamicFlee
+		{
+            Character = redKinematicData,
+			Target = this.GreenCharacter.KinematicData,
+			MaxAcceleration = MAX_ACCELERATION
+		};
+
+	    this.RedDynamicWander = new DynamicWander
+        {
+            MaxAcceleration = MAX_ACCELERATION
+        };
+
+        this.RedDynamicArrive = new DynamicArrive
+        {
+            Character = this.RedCharacter.KinematicData,
+            Target = this.GreenCharacter.KinematicData,
+            MaxAcceleration = MAX_ACCELERATION
+        };
+
+        this.GreenDynamicSeek = new DynamicSeek
+        {
+            Character = greenKinematicData,
+            Target = this.RedCharacter.KinematicData,
+            MaxAcceleration = MAX_ACCELERATION
+        };
+
+        this.GreenDynamicFlee = new DynamicFlee
+        {
+            Character = greenKinematicData,
+            Target = this.RedCharacter.KinematicData,
+            MaxAcceleration = MAX_ACCELERATION
+        };
+
+        this.GreenDynamicWander = new DynamicWander
+        {
+            MaxAcceleration = MAX_ACCELERATION
+        };
+
+        this.GreenDynamicArrive = new DynamicArrive
+        {
+            Character = this.GreenCharacter.KinematicData,
+            Target = this.RedCharacter.KinematicData,
+            MaxAcceleration = MAX_ACCELERATION
+        };
+
+        #endregion
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Q)) 
+		{
+			this.RedCharacter.Movement = null;
+		} 
+		else if (Input.GetKeyDown (KeyCode.W))
+		{
+		    this.RedCharacter.Movement = this.RedDynamicSeek;
+		}
+		else if (Input.GetKeyDown (KeyCode.E))
+		{
+		    this.RedCharacter.Movement = this.RedDynamicFlee;
+		}
+		else if (Input.GetKeyDown(KeyCode.R))
+        {
+            this.RedCharacter.Movement = this.RedDynamicArrive;
+        }
+		else if (Input.GetKeyDown (KeyCode.T))
+		{
+		    this.RedCharacter.Movement = this.RedDynamicWander;
+		}
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.GreenCharacter.Movement = null;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.GreenCharacter.Movement = this.GreenDynamicSeek;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            this.GreenCharacter.Movement = this.GreenDynamicFlee;
+        }
+        else if(Input.GetKeyDown(KeyCode.F))
+        {
+            this.GreenCharacter.Movement = this.GreenDynamicArrive;
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
+            this.GreenCharacter.Movement = this.GreenDynamicWander;
+        }
+
+        this.UpdateMovingGameObject(this.RedCharacter);
+        this.UpdateMovingGameObject(this.GreenCharacter);
+
+	    if (this.redDebugTarget != null && this.RedCharacter.Movement != null)
+	    {
+	        this.redDebugTarget.transform.position = this.RedCharacter.Movement.Target.position;
+	    }
+
+	    if (this.greenDebugTarget != null && this.GreenCharacter.Movement != null)
+	    {
+	        this.greenDebugTarget.transform.position = this.GreenCharacter.Movement.Target.position;
+	    }
+
+	    this.UpdateMovementText();
+	}
+
+    private void UpdateMovingGameObject(DynamicCharacter movingCharacter)
+    {
+        if (movingCharacter.Movement != null)
+        {
+            movingCharacter.Update();
+            movingCharacter.KinematicData.ApplyWorldLimit(X_WORLD_SIZE,Z_WORLD_SIZE);
+            movingCharacter.GameObject.transform.position = movingCharacter.KinematicData.position;
+        }
+    }
+
+    private void UpdateMovementText()
+    {
+        if (this.GreenCharacter.Movement == null)
+        {
+            this.GreenMovementText.text = "Green Movement: Stationary";
+        }
+        else
+        {
+            this.GreenMovementText.text = "Green Movement: " + this.GreenCharacter.Movement.Name;
+        }
+
+        if (this.RedCharacter.Movement == null)
+        {
+            this.RedMovementText.text = "Red Movement: Stationary";
+        }
+        else
+        {
+            this.RedMovementText.text = "Red Movement: " + this.RedCharacter.Movement.Name;
+        }
+    }
+}
---
+++ Assets/Scripts/KinematicManager.cs
@@ -0,0 +1,168 @@
+﻿using Assets.Scripts.IAJ.Unity.Movement.KinematicMovement;
+using Assets.Scripts.IAJ.Unity.Util;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KinematicManager : MonoBehaviour
+{
+    public const float X_WORLD_SIZE = 55;
+    public const float Z_WORLD_SIZE = 32.5f;
+    private const float MAX_SPEED = 10.0f;
+    private const float TIME_TO_TARGET = 2.0f;
+    private const float RADIUS = 1.0f;
+    private const float MAX_ROTATION = 8*MathConstants.MATH_PI;
+
+	private KinematicCharacter RedCharacter { get; set; }
+	private KinematicCharacter GreenCharacter { get; set; }
+
+    private Text RedMovementText { get; set; }
+
+    private Text GreenMovementText { get; set; }
+
+	// Use this for initialization
+	void Start () 
+	{
+		var textObj = GameObject.Find ("InstructionsText");
+		if (textObj != null) 
+		{
+			textObj.GetComponent<Text>().text = 
+				"Instructions\n\n" +
+				"Red Character\n" +
+				"Q - Stationary\n" +
+				"W - Seek\n" + 
+				"E - Flee\n" + 
+				"R - Arrive\n" + 
+				"T - Wander\n\n" +  
+				"Green Character\n" + 
+				"A - Stationary\n" +
+				"S - Seek\n" +
+				"D - Flee\n" + 
+				"F - Arrive\n" +
+				"G - Wander\n"; 
+		}
+
+		var redObj = GameObject.Find ("Red");
+		if(redObj != null) this.RedCharacter = new KinematicCharacter(redObj);
+		var greenObj = GameObject.Find ("Green");
+		if (greenObj != null) this.GreenCharacter = new KinematicCharacter(greenObj);
+
+	    this.RedMovementText = GameObject.Find("RedMovement").GetComponent<Text>();
+	    this.GreenMovementText = GameObject.Find("GreenMovement").GetComponent<Text>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Q)) 
+		{
+			this.RedCharacter.Movement = null;
+		} 
+		else if (Input.GetKeyDown (KeyCode.W)) 
+		{
+            this.RedCharacter.Movement = new KinematicSeek
+			{
+                Target = this.GreenCharacter.StaticData,
+			    MaxSpeed = MAX_SPEED
+			};
+		}
+		else if (Input.GetKeyDown (KeyCode.E)) 
+		{
+            this.RedCharacter.Movement = new KinematicFlee
+			{
+                Target = this.GreenCharacter.StaticData,
+				MaxSpeed = MAX_SPEED
+			};
+		}
+		else if (Input.GetKeyDown (KeyCode.R)) 
+		{
+            this.RedCharacter.Movement = new KinematicArrive
+			{
+                Target = this.GreenCharacter.StaticData,
+                MaxSpeed = MAX_SPEED,
+                TimeToTarget = TIME_TO_TARGET,
+                Radius = RADIUS
+			};
+		}
+		else if (Input.GetKeyDown (KeyCode.T)) 
+		{
+            this.RedCharacter.Movement = new KinematicWander
+			{
+				MaxRotation = MAX_ROTATION,
+				MaxSpeed = MAX_SPEED
+			};
+		}
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.GreenCharacter.Movement = null;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.GreenCharacter.Movement = new KinematicSeek
+            {
+                Target = this.RedCharacter.StaticData,
+                MaxSpeed = MAX_SPEED
+            };
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            this.GreenCharacter.Movement = new KinematicFlee
+            {
+                Target = this.RedCharacter.StaticData,
+                MaxSpeed = MAX_SPEED
+            };
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            this.GreenCharacter.Movement = new KinematicArrive
+            {
+                Target = this.RedCharacter.StaticData,
+                MaxSpeed = MAX_SPEED,
+                TimeToTarget = TIME_TO_TARGET,
+                Radius = RADIUS
+            };
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
+            this.GreenCharacter.Movement = new KinematicWander
+            {
+                MaxRotation = MAX_ROTATION,
+                MaxSpeed = MAX_SPEED
+            };
+        }
+
+        this.UpdateMovingGameObject(this.RedCharacter);
+        this.UpdateMovingGameObject(this.GreenCharacter);
+
+        this.UpdateMovementText();
+	}
+
+    private void UpdateMovingGameObject(KinematicCharacter movingCharacter)
+    {
+        if (movingCharacter.Movement != null)
+        {
+            movingCharacter.Update();
+            movingCharacter.StaticData.ApplyWorldLimit(X_WORLD_SIZE,Z_WORLD_SIZE);
+            movingCharacter.GameObject.transform.position = movingCharacter.StaticData.position;
+        }
+    }
+
+    private void UpdateMovementText()
+    {
+        if (this.GreenCharacter.Movement == null)
+        {
+            this.GreenMovementText.text = "Green Movement: Stationary";
+        }
+        else
+        {
+            this.GreenMovementText.text = "Green Movement: " + this.GreenCharacter.Movement.Name;
+        }
+
+        if (this.RedCharacter.Movement == null)
+        {
+            this.RedMovementText.text = "Red Movement: Stationary";
+        }
+        else
+        {
+            this.RedMovementText.text = "Red Movement: " + this.RedCharacter.Movement.Name;
+        }
+    }
+}
